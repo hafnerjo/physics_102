b2a58c409f7da72541db6417a3627a0fbf8d74db
halfner <johnhafner44@gmail.com>
2020-10-26 15:14:19 -0400

    Initial commit

---
+++ internal_resistance.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 26 10:49:22 2020
+
+@author: John
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+voltage_drops = np.array([2.6, 4.99, 4.62, 4.83, 5.09, 5.28, 5.31, 5.60])
+resistances = np.array([10, 30, 50, 100, 150, 200, 300, 500])
+current = voltage_drops / resistances
+
+plt.plot(current, voltage_drops, 'ro')
+
+m, b = np.polyfit(current, voltage_drops, 1)
+
+print('m=', m)
+print('b=', b)
+
+x = np.linspace(0, 0.3)
+y = m*x + b
+
+plt.plot(x, y, 'g--')
+
+plt.show()
+
+print(voltage_drops)
+print(current)
